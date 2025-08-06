c8f4ad87f284f3a0298b308d0fe38d2d2fa3f4f7
abenezeryohanes <abenezeryohannes12@gmail.com>
2025-08-06 16:04:27 +0300

    Add division function module and update index.js to demonstrate usage

---
+++ Phase 3_ Node_ Express_ MySql and Reactjs/ES6/ES6_exported_module.js
@@ -0,0 +1,7 @@
+// Default export for division function
+export default function division(a, b) {
+    if (b === 0) {
+        return "Division by zero is not allowed";
+    }
+    return a / b;
+}
---
+++ Phase 3_ Node_ Express_ MySql and Reactjs/ES6/index.js
@@ -0,0 +1,7 @@
+import div from './ES6_exported_module.js';
+
+let num1 = 10;
+let num2 = 5;
+
+console.log(`The values of num1 and num2 are: ${num1}, ${num2}`);
+console.log(`The result of division is: ${div(num1, num2)}`);
